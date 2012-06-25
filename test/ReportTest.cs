2590e7244fe957f16b995eab798adfe635d36471
unknown <jffu@jffu-PC.(none)>
2012-06-25 20:26:50 +0800

    added more tests for report

--- test/ReportTest.cs
+++ test/ReportTest.cs
@@ -88,5 +88,126 @@ namespace ParkingLot
                                  + "		parkinglot: 3\r\n";
             Assert.AreEqual(expectedReport, report);
         }
+
+        [Test]
+        public void managers_with_one_parkinglot_and_one_parking_boy_should_print_its_avaliable_position()
+        {
+            var manager = new Manager();
+            manager.Manage(new Parkinglot(1));
+
+            var parkingBoy = new ParkingBoy(new MaxAvailableRate());
+            parkingBoy.Manage(new Parkinglot(2));
+            manager.Manage(parkingBoy);
+
+            var report = manager.Print(0);
+            var expectedReport = "manager:\r\n"
+                                 + "	parkinglot: 1\r\n"
+                                 + "	parkingboy:\r\n"
+                                 + "		parkinglot: 2\r\n";
+            Assert.AreEqual(expectedReport, report);
+        }
+
+        [Test]
+        public void managers_with_two_parkinglots_and_two_parking_boys_should_print_its_avaliable_position()
+        {
+            var manager = new Manager();
+            manager.Manage(new Parkinglot(1));
+            manager.Manage(new Parkinglot(2));
+
+            var parkingBoy = new ParkingBoy(new MaxAvailableRate());
+            parkingBoy.Manage(new Parkinglot(2));
+            manager.Manage(parkingBoy);
+
+            var parkingBoy1 = new ParkingBoy(new MaxAvailableRate());
+            parkingBoy1.Manage(new Parkinglot(2));
+            parkingBoy1.Manage(new Parkinglot(3));
+            manager.Manage(parkingBoy1);
+            var report = manager.Print(0);
+            var expectedReport = "manager:\r\n"
+                                 + "	parkinglot: 1\r\n"
+                                 + "	parkinglot: 2\r\n"
+                                 + "	parkingboy:\r\n"
+                                 + "		parkinglot: 2\r\n"
+                                 + "	parkingboy:\r\n"
+                                 + "		parkinglot: 2\r\n"
+                                 + "		parkinglot: 3\r\n";
+            Assert.AreEqual(expectedReport, report);
+        }
+
+        [Test]
+        public void managers_with_one_manager_should_print_its_avaliable_position()
+        {
+            var manager = new Manager();
+            var manager1 = new Manager();
+            manager1.Manage(new Parkinglot(1));
+            manager1.Manage(new Parkinglot(2));
+
+            var parkingBoy = new ParkingBoy(new MaxAvailableRate());
+            parkingBoy.Manage(new Parkinglot(2));
+            manager1.Manage(parkingBoy);
+
+            var parkingBoy1 = new ParkingBoy(new MaxAvailableRate());
+            parkingBoy1.Manage(new Parkinglot(2));
+            parkingBoy1.Manage(new Parkinglot(3));
+            manager1.Manage(parkingBoy1);
+
+            manager.Manage(manager1);
+            
+            var report = manager.Print(0);
+            var expectedReport = "manager:\r\n"
+                                 + "	manager:\r\n"
+                                 + "		parkinglot: 1\r\n"
+                                 + "		parkinglot: 2\r\n"
+                                 + "		parkingboy:\r\n"
+                                 + "			parkinglot: 2\r\n"
+                                 + "		parkingboy:\r\n"
+                                 + "			parkinglot: 2\r\n"
+                                 + "			parkinglot: 3\r\n";
+            Assert.AreEqual(expectedReport, report);
+        }
+
+        [Test]
+        public void manager_with_two_managers_should_print_its_avaliable_position()
+        {
+            var manager = new Manager();
+            manager.Manage(new Manager());
+            manager.Manage(new Manager());
+
+            var report = manager.Print(0);
+
+            var expectedReport = "manager:\r\n"
+                                 + "	manager:\r\n"
+                                 + "	manager:\r\n";
+
+            Assert.AreEqual(expectedReport,report);
+        }
+
+        [Test]
+        public void manager_with_manager_and_parkingboy_and_parkinglot_should_print_its_avaliable_position()
+        {
+            var manager = new Manager();
+            var manager1 = new Manager();
+            var parkingBoy = new ParkingBoy(new MaxAvailableRate());
+            parkingBoy.Manage(new Parkinglot(1));
+            manager1.Manage(parkingBoy);
+            manager.Manage(manager1);
+
+            var parkingBoy1 = new ParkingBoy(new AnyNotFull());
+            parkingBoy1.Manage(new Parkinglot(2));
+            manager.Manage(parkingBoy1);
+            
+            manager.Manage(new Parkinglot(3));
+
+            var report = manager.Print(0);
+
+            var expectedReport = "manager:\r\n"
+                                 + "	manager:\r\n"
+                                 + "		parkingboy:\r\n"
+                                 + "			parkinglot: 1\r\n"
+                                 + "	parkingboy:\r\n"
+                                 + "		parkinglot: 2\r\n"
+                                 + "	parkinglot: 3\r\n";
+            Assert.AreEqual(expectedReport, report);
+        }
     }
 }
